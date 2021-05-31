1f670977f8ea8ded9331db568c607b9744b84e53
JUkhan <jasim.uddin.khan@gmail.com>
2021-05-31 20:34:48 +0600

    1 file deleted

--- src/app/states/store.ts
+++
@@ -1,5 +0,0 @@
-import { createStore } from "mono-state";
-import { todos } from "./todos";
-import { visibilittyFilter } from "./visibilityFilter";
-
-export const store = createStore([todos, visibilittyFilter]);
